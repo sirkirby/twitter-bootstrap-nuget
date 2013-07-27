90e3c83438914d91d90c520d3f845daafd824b85
Chris Kirby <chrisk@ryantechinc.com>
2013-07-27 16:01:42 -0700

    update all source to v3 rc1
    created rc1 versions of all three packages. introducing new breaking
    change to less package where source files are now contained within the
    ~/content/bootstrap directory instead of the ~/content/less directory.
    updated less.js to latest release.

--- Twitter.Bootstrap.MVC/BootstrapBundleConfig.cs.pp
+++ Twitter.Bootstrap.MVC/BootstrapBundleConfig.cs.pp
@@ -12,7 +12,7 @@ namespace $rootnamespace$.App_Start
 			// Add @Scripts.Render("~/bundles/bootstrap") after jQuery in your _Layout.cshtml view
 			// When <compilation debug="true" />, MVC4 will render the full readable version. When set to <compilation debug="false" />, the minified version will be rendered automatically
 			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap*"));
-			BundleTable.Bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css", "~/Content/bootstrap-responsive.css"));
+			BundleTable.Bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css"));
 		}
 	}
 }
