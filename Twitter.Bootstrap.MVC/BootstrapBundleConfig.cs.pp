304fe9d3988e9f1fe197f74d10d9f291ee5bc119
Chris Kirby <chrisk@ryantechinc.com>
2013-07-27 18:13:03 -0700

    added glyphicons and reworked package structure
    moved the main compiled package to the ~/Content/bootstrap structure so
    that the relative dependency on the glyphicons fonts directory can be
    shared by both the less and compiled packages. still all rc1 source.

--- Twitter.Bootstrap.MVC/BootstrapBundleConfig.cs.pp
+++ Twitter.Bootstrap.MVC/BootstrapBundleConfig.cs.pp
@@ -8,11 +8,11 @@ namespace $rootnamespace$.App_Start
 	{
 		public static void RegisterBundles()
 		{
-			// Add @Styles.Render("~/Content/bootstrap") in the <head/> of your _Layout.cshtml view
+			// Add @Styles.Render("~/Content/bootstrap/css") in the <head/> of your _Layout.cshtml view
 			// Add @Scripts.Render("~/bundles/bootstrap") after jQuery in your _Layout.cshtml view
 			// When <compilation debug="true" />, MVC4 will render the full readable version. When set to <compilation debug="false" />, the minified version will be rendered automatically
-			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap*"));
-			BundleTable.Bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css"));
+			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
+			BundleTable.Bundles.Add(new StyleBundle("~/Content/bootstrap/css").Include("~/Content/bootstrap/bootstrap.css", "~/Content/bootstrap/bootstrap-glyphicons.css"));
 		}
 	}
 }
